0ffb5fe7b3152c7a2aecc96a2127af34d1f5b243
Lennon McLean <lmmclean08@gmail.com>
2024-02-06 23:18:52 -0500

    important fix

--- drawing.h
+++ drawing.h
@@ -1,6 +1,6 @@
 void plot(int x, int y, int color);
 void clear();
-void dBuffer(int x, int y, int width, int height, int depth, char* buffer);
+void buffer(int x, int y, int width, int height, int depth, char* buffer);
 void rectangle(int x, int y, int width, int height, int color);
 void circle(int x, int y, int radius, int color);
 void line(int x0, int y0, int x1, int y1, int color);
